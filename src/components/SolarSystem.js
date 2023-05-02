5370fa78ff948561ee151d27e168edd0dcf5f8e7
ThyeKatsivalis <titomalb@gmail.com>
2023-05-02 14:03:33 -0300

    subindo projeto solar system em atualização

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
+
+class SolarSystem extends Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        <Title headline="Planetas" />
+        {planets.map(({ name, image }) => (
+          <PlanetCard
+            planetName={ name }
+            planetImage={ image }
+            key={ name }
+          />
+        ))}
+      </div>
+    );
+  }
+}
+export default SolarSystem;
